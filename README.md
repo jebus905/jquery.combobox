9f141bc70aa9a2d7e3ef3afcf217e98754a65dd2
Jason Powell <jasondpowell@gmail.com>
2014-03-18 11:43:37 -0400

    Primary commit
    The primary push.  Fixes and improvements are to come.

---
+++ README.md
@@ -0,0 +1,53 @@
+#jQuery Combobox Widget
+
+A comobox widget based on the jQuery UI autocomplete demo
+
+##Features
+
+* works with jQuery 1.8+
+
+##Usage
+
+Basic usage:
+
+    $('select').combobox( {options} );
+
+##Parameters
+
+###Options
+
+####`forceUcase`
+
+`forceUcase` will force any entered data to uppercase. Boolean value. Default = false
+
+####`invalidAction`
+
+`invalidAction` Dictates the action to perform if an item not already in the list of selectable items is entered. String value. Default = "remove".  
+Available values:
+
+| invalidAction | Action Description                                        |
+| ------------- | --------------------------------------------------------- |
+| add           | New value is added to the bottom of the list.             |
+| remove        | Value is removed (cleared out) and an error is displayed. |
+
+####`classPrefix`
+
+`classPrefix` The prefix used to apply to the classes of all elements involved. String value. Default = "ui"
+The classes involved include:
+* wrapper gets `prefix`-combobox
+* input box gets `prefix`-combobox-input
+* button gets `prefix`-combobox-toggle
+	
+###Methods
+
+####`enable`
+
+Will enable the combobox.
+
+####`disable`
+
+Will disable the combobox.
+
+####`destroy`
+
+Will destroy all combobox elements and restore the original select element.
---
+++ jquery.combobox.js
@@ -0,0 +1,175 @@
+(function( $ ) {
+	$.widget( "custom.combobox", {
+		options: {
+			forceUcase: false,
+			invalidAction: "remove",
+			classPrefix: "ui"
+		},
+		
+		_create: function() {
+			this.wrapper = $( "<span>" )
+				.addClass( this.options.classPrefix + "-combobox" )
+				.insertAfter( this.element );
+			
+			this.element.hide();
+			this._createAutocomplete();
+			this._createShowAllButton();
+		},
+	
+		_createAutocomplete: function() {
+			var selected = this.element.children( ":selected" ),
+				value = selected.val() ? selected.text() : "";
+			
+			this.input = $( "<input>" )
+				.appendTo( this.wrapper )
+				.val( value )
+				.attr( "title", "" )
+				.addClass( this.options.classPrefix + "-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
+				.autocomplete({
+					delay: 0,
+					minLength: 0,
+					source: $.proxy( this, "_source" )
+				})
+				.tooltip({
+					tooltipClass: "ui-state-highlight"
+				});
+			
+			this._on( this.input, {
+				autocompleteselect: function( event, ui ) {
+					ui.item.option.selected = true;
+					this._trigger( "select", event, {
+						item: ui.item.option
+					});
+				},
+		
+				autocompletechange: "_removeIfInvalid"
+			});
+		},
+	
+		_createShowAllButton: function() {
+			var input = this.input,
+				wasOpen = false;
+		
+			$( "<a>" )
+				.attr( "tabIndex", -1 )
+				.attr( "title", "Show All Items" )
+				.tooltip()
+				.appendTo( this.wrapper )
+				.button({
+					icons: {
+						primary: "ui-icon-triangle-1-s"
+					},
+					text: false
+				})
+				.removeClass( "ui-corner-all" )
+				.addClass( this.options.classPrefix + "-combobox-toggle ui-corner-right" )
+				.mousedown(function() {
+					wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+				})
+				.click(function() {
+					input.focus();
+		
+					// Close if already visible
+					if ( wasOpen ) {
+						return;
+					}
+		
+					// Pass empty string as value to search for, displaying all results
+					input.autocomplete( "search", "" );
+				});
+		},
+	
+		_source: function( request, response ) {
+			var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+			response( this.element.children( "option" ).map(function() {
+				var text = $( this ).text();
+				if ( this.value && ( !request.term || matcher.test(text) ) )
+				return {
+					label: text,
+					value: text,
+					option: this
+				};
+			}) );
+		},
+	
+		_removeIfInvalid: function( event, ui ) {
+			if(this.options.forceUcase) this.input.val(this.input.val().toUpperCase());
+			
+			// Selected an item, nothing to do
+			if ( ui.item ) {
+				return;
+			}
+		
+			// Search for a match (case-insensitive)
+			var value = this.input.val(),
+				valueLowerCase = value.toLowerCase(),
+				valid = false;
+			this.element.children( "option" ).each(function() {
+				if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+					this.selected = valid = true;
+					return false;
+				}
+			});
+		
+			// Found a match, nothing to do
+			if ( valid ) {
+				return;
+			}
+			
+			// So, no match was found.  What we do next depends on options.invalidAction.
+			switch(this.options.invalidAction.toLowerCase()) {
+				case "remove":
+					// Remove invalid value
+					this.input
+						.val( "" )
+						.attr( "title", value + " didn't match any item" )
+						.tooltip( "open" );
+					this.element.val( "" );
+					this._delay(function() {
+						this.input.tooltip( "close" ).attr( "title", "" );
+					}, 2500 );
+					this.input.data( "ui-autocomplete" ).term = "";
+					break;
+				
+				case "add":
+					// Add new (non blank) value to list if it hasn't been found
+					if(value.replace(/\s/g, "") != "") {
+						var option = $("<option>")
+								.html(value)
+								.text(value)
+								.val(value)
+								.appendTo(this.element);
+						this.element.children(option).prop("selected", true).change();
+					}
+					break;
+			}
+		},
+		
+		_destroy: function() {
+			this.wrapper.remove();
+			this.element.show();
+		},
+		
+		destroy: function() {
+			this.wrapper.remove();
+			this.element.show();
+			//$.Widget.prototype.destroy.call( this );
+		},
+		
+		disable: function() {
+			this.input.autocomplete("disable");							// disable the autocomplete element
+			this.input.prop("disabled", true);							// disable the input field
+			this.element.prop("disabled", true);						// disable the original select element
+			this.input.addClass("ui-state-disabled");					// add disabled state to input field
+			this.wrapper.find("a").addClass("ui-state-disabled");		// add disabled state to show all indicator
+		},
+		
+		enable: function() {
+			this.input.autocomplete("enable");							// enable the autocomplete element
+			this.input.prop("disabled", false);							// enable the input field
+			this.element.prop("disabled", false);						// enable the original select element
+			this.input.removeClass("ui-state-disabled");				// remove disabled state from input field
+			this.wrapper.find("a").removeClass("ui-state-disabled");	// remove disabled state from show all indicator
+		}
+	});
+})( jQuery );
